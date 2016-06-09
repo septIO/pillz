9ec7f3a0e177e21d470ed38df36b8bfe56177b7e
septIO <in@sept.io>
2016-06-09 12:21:54 +0200

    fixed food check to check food instead of position

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ var updateGamestate = function (callable) {
     for (i = food.length - 1; i >= 0; i--) {
         for (j = 0; j < players.length; j++) {
             var p = players[j];
-            if (typeof food[i].position === 'undefined') return;
+            if (typeof food[i] === 'undefined') return;
             var f = new Vector(food[i].position.x, food[i].position.y);
             var dist = p.position.midpoint(p.size).subtract(f).length();
             if (dist < p.record) {
